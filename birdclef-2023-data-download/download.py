749756322d5b7ac216a1df08497b3f233276acaf
Dileep Jayamal <dileep.jayamal@gmail.com>
2023-03-28 15:55:39 +0530

    window hidden

--- birdclef-2023-data-download/download.py
+++ birdclef-2023-data-download/download.py
@@ -19,10 +19,13 @@ def process_results(results):
             try:
                 es = class_.find_element(By.XPATH,'..').find_element(By.XPATH,'..').find_element(By.XPATH,'..').find_elements(By.TAG_NAME, "a")
                 es[0].click()
+
             except Exception as ex:
                 print(ex)
         else:
+            time.sleep(20)
             return False
+    time.sleep(20)
     return True
 
 
@@ -31,16 +34,15 @@ def process_class(class_name):
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' \
                  '(KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
     chrome_options.add_argument(f'user-agent={user_agent}')
-    #chrome_options.add_argument("--headless")
-    #chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("start-maximized")
-    #chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-extensions")
     driver = webdriver.Chrome(options=chrome_options)
     driver.get("https://xeno-canto.org/species/{}".format(class_name))
     time.sleep(1)
     current_page = 0
     while True:
-        time.sleep(20)
         wait = WebDriverWait(driver, 10)
         wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "results")))
         results = driver.find_element(By.CLASS_NAME, "results")
